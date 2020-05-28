4fc36539b0810e3e5772a478a15382d35b8ee567
EgorGo23 <egor.go2303@gmail.com>
2020-05-28 20:58:04 +0300

    unknown

--- routes/add.js
+++ routes/add.js
@@ -1,19 +1,20 @@
-const { Router } = require('express');
-const router = Router();
-const Course = require('../models/course');
+const {Router} = require('express')
+const Course = require('../models/course')
+const router = Router()
 
 router.get('/', (req, res) => {
-    res.render('add', {
-        title: 'Добавить курс',
-        isAdd: true,
-    });
+  res.render('add', {
+    title: 'Добавить курс',
+    isAdd: true
+  })
 })
 
-router.post('/', (req, res) => {
-    const course = new Course(req.body.title, req.body.price, req.body.img);
-    course.save();
+router.post('/', async (req, res) => {
+  const course = new Course(req.body.title, req.body.price, req.body.img)
 
-    res.redirect('/courses');
+  await course.save()
+
+  res.redirect('/courses')
 })
 
-module.exports = router;
+module.exports = router
