2f00b47cbfe0c728c137b3e913931b345a0f3bb4
EgorGo23 <egor.go2303@gmail.com>
2020-05-29 21:15:58 +0300

    connect mongodb

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const path = require('path')
+const mongoose = require('mongoose');
 const exphbs = require('express-handlebars')
 const homeRoutes = require('./routes/home')
 const cardRoutes = require('./routes/card')
 const addRoutes = require('./routes/add')
 const coursesRoutes = require('./routes/courses')
+
 
 const app = express()
 
@@ -17,7 +19,7 @@ app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended: true}))
 
 app.use('/', homeRoutes)
@@ -25,8 +27,22 @@ app.use('/add', addRoutes)
 app.use('/courses', coursesRoutes)
 app.use('/card', cardRoutes)
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
+
+async function start() {
+  try {
+    const url = 'mongodb+srv://egor:imT1GRtTcOXtUy3w@cluster0-68igm.mongodb.net/test?retryWrites=true&w=majority';
+    await mongoose.connect(url, { useNewUrlParser: true });
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`)
+    })
+  } catch(e) {
+    console.log(e);
+  }
+}
+
+start();
+
+
+
